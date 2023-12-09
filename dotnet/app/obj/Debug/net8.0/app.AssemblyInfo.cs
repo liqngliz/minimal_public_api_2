632eefc01027f2c297be280d98527f072cbc60e3
liqngliz <liqngliz@gmail.com>
2023-12-09 12:07:58 +0100

    added simple border styling to traffic lights

--- dotnet/app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ dotnet/app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f1600797db28aa2b612de179a73a0ad893e7bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
